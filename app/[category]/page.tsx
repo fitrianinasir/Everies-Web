1a5fe8c51afd64518c6563ab4842650be41dfecd
Fitriani Nasir <fitrianinasir8@gmail.com>
2023-12-10 19:08:07 +0700

    shopping page 70%

--- app/[category]/page.tsx
+++ app/[category]/page.tsx
@@ -2,11 +2,11 @@
 import React, { useState } from "react";
 import { useParams } from "next/navigation";
 import Image from "next/image";
-import styles from './category.module.css'
+import styles from "./category.module.css";
 
 const CategoryPage = () => {
   const { category } = useParams();
-  const [isDropDownOpen, setDropDownOpen] = useState(false)
+  const [isDropDownOpen, setDropDownOpen] = useState(false);
 
   const handleDropDown = () => {
     setDropDownOpen(!isDropDownOpen);
@@ -29,7 +29,7 @@ const CategoryPage = () => {
           <h2 className="self-end">Categories</h2>
           <h2 className="self-end">Profile</h2>
           <div className="pr-10 md:pr-20">
-            <span className="w-4 rounded-full bg-red-600 text-xs fixed top-12">
+            <span className="w-4 rounded-full bg-red-600 text-xs top-12">
               4
             </span>
             <Image
@@ -45,7 +45,7 @@ const CategoryPage = () => {
       <div className="w-full h-1/4 clear-right bg-[url('/images/women-cover.jpg')] bg-cover"></div>
       <div className="w-full h-8 bg-[#113946]"></div>
       <hr className=" h-px my-8 bg-gray-200 border-xs dark:bg-gray-500"></hr>
-      <div className={`w-full flex fixed ${styles.filters}`}>
+      <div className={`w-full flex ${styles.filters}`}>
         <div className="mr-2">
           <button
             id="dropdownUsersButton"
@@ -74,7 +74,9 @@ const CategoryPage = () => {
           </button>
           <div
             id="dropdownUsers"
-            className={`z-10 fixed ${isDropDownOpen? "block" : "hidden"} bg-white divide-y divide-gray-100 rounded-lg shadow w-44  left-32 mt-3`}
+            className={`z-10 fixed ${
+              isDropDownOpen ? "block" : "hidden"
+            } bg-white divide-y divide-gray-100 rounded-lg shadow w-44  left-32 mt-3`}
           >
             <ul
               className="py-2 text-xs text-gray-700 h-36 overflow-y-auto"
@@ -128,6 +130,172 @@ const CategoryPage = () => {
           className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-full flex w-36 h-6 ps-5 "
           placeholder="Search"
         />
+      </div>
+      <div className="w-full grid grid-cols-2 gap-4 p-7">
+        {/* center a div on another div */}
+        <div className="">
+          <div className="w-full h-60 bg-[url('/images/sample1.jpg')] bg-cover"></div>
+          <div className="w-full h-16 pt-2">
+            <h1 className="text-xs text-[#BCA37F]">Arabella Shirt - White</h1>
+            <h2 className="text-sm font-bold">Rp. 225.000</h2>
+            <div className="flex mt-1">
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <p className={`ml-1 ${styles.reviewText}`}>3.1k Sold</p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div className="w-full h-60 bg-[url('/images/sample2.jpg')] bg-cover"></div>
+          <div className="w-full h-16 pt-2">
+            <h1 className="text-xs text-[#BCA37F]">
+              Jasmine Shirt - Strip Blue White{" "}
+            </h1>
+            <h2 className="text-sm font-bold">Rp. 225.000</h2>
+            <div className="flex mt-1">
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <p className={`ml-1 ${styles.reviewText}`}>3.1k Sold</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="w-full h-60 bg-[url('/images/sample3.jpg')] bg-cover"></div>
+          <div className="w-full h-16 pt-2">
+            <h1 className="text-xs text-[#BCA37F]">Diana Shirt - Orange</h1>
+            <h2 className="text-sm font-bold">Rp. 225.000</h2>
+            <div className="flex mt-1">
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <p className={`ml-1 ${styles.reviewText}`}>3.1k Sold</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="w-full h-60 bg-[url('/images/sample4.jpg')] bg-cover"></div>
+          <div className="w-full h-16 pt-2">
+            <h1 className="text-xs text-[#BCA37F]">Zea - One Set Outfit</h1>
+            <h2 className="text-sm font-bold">Rp. 225.000</h2>
+            <div className="flex mt-1">
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <img
+                width="10"
+                src="https://img.icons8.com/ios-filled/50/F1B4BB/star--v1.png"
+                alt="star--v1"
+                className="ml-0.5"
+              />
+              <p className={`ml-1 ${styles.reviewText}`}>3.1k Sold</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
